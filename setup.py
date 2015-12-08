5faeeb2540e12d4092ad2845c044ad4585f3a8bf
olivier R-D <olivier@loke>
2015-12-08 22:59:23 +0100

    new release

--- setup.py
+++ setup.py
@@ -2,19 +2,15 @@ from setuptools import setup
 from distutils.command.install_data import install_data
 
 
-
-
-setup (name = "freeopcua-client", 
-        version = "0.3.4",
-        description = "Minimal OPC-UA Client GUI",
-        author = "Olivier R-D",
-        url = 'https://github.com/FreeOpcUa/opcua-client-gui',
-        packages=["freeopcuaclient"],
-        license = "GNU General Public License",
-        install_requires = ["freeopcua"],
-        entry_points = {'console_scripts': 
-                ['freeopcua-client = freeopcuaclient.mainwindow:main']
-                }
-        )
-
-
+setup(name="freeopcua-client",
+      version="0.3.5",
+      description="Minimal OPC-UA Client GUI",
+      author="Olivier R-D",
+      url='https://github.com/FreeOpcUa/opcua-client-gui',
+      packages=["freeopcuaclient"],
+      license="GNU General Public License",
+      install_requires=["freeopcua"],
+      entry_points={'console_scripts':
+                    ['freeopcua-client = freeopcuaclient.mainwindow:main']
+                    }
+      )
