8931c04e11e88f026c7ec91b8d64470d26fa351e
olivier R-D <olivier@loke>
2015-12-30 10:12:03 +0100

    new release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.command.install_data import install_data
 
 
 setup(name="freeopcua-client",
-      version="0.4.1",
+      version="0.4.2",
       description="Minimal OPC-UA Client GUI",
       author="Olivier R-D",
       url='https://github.com/FreeOpcUa/opcua-client-gui',
