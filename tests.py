44c72f5556c73e15c3ee2698bea297e32e001a81
olivier R-D <olivier@loke>
2016-12-04 11:52:02 +0100

    add test for server node

--- tests.py
+++ tests.py
@@ -38,15 +38,29 @@ class TestClient(unittest.TestCase):
         item = self.client.attrs_ui.model.itemFromIndex(idx)
         return item.data(Qt.UserRole).value
 
-
     def test_select_objects(self):
         objects = self.server.nodes.objects
         self.client.tree_ui.set_current_node("Objects")
         self.assertEqual(objects, self.client.tree_ui.get_current_node())
         self.assertGreater(self.client.attrs_ui.model.rowCount(), 6)
+        print("REF COUNT", self.client.refs_ui.model.rowCount())
+        self.assertGreater(self.client.refs_ui.model.rowCount(), 1)
 
         data = self.get_attr_value("NodeId")
         self.assertEqual(data, objects.nodeid)
+
+    def test_select_server_node(self):
+        self.client.tree_ui.set_current_node("Objects")
+        self.client.tree_ui.expand_current_node()  # must be expanded to load children
+        server_node = self.server.nodes.server
+        self.client.tree_ui.set_current_node("Server")
+        self.assertEqual(server_node, self.client.tree_ui.get_current_node())
+        self.assertGreater(self.client.attrs_ui.model.rowCount(), 6)
+        self.assertGreater(self.client.refs_ui.model.rowCount(), 10)
+
+        data = self.get_attr_value("NodeId")
+        self.assertEqual(data, server_node.nodeid)
+
 
 
 if __name__ == "__main__":
