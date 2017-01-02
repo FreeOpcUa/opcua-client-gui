4beaa7874c3bd02f235985f9bf477341a904547c
olivier R-D <olivier@loke>
2017-01-02 10:17:11 +0100

    new release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="opcua-client",
-      version="0.4.5",
+      version="0.5.0",
       description="Minimal OPC-UA Client GUI",
       author="Olivier R-D",
       url='https://github.com/FreeOpcUa/opcua-client-gui',
