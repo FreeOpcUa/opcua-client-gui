a5383e356304f58700df9162bf8ef8858bcec045
oroulet <olivier@loke>
2018-04-03 09:23:35 +0200

    new release

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 
 setup(name="opcua-client",
-      version="0.7.1",
+      version="0.7.2",
       description="Minimal OPC-UA Client GUI",
       author="Olivier R-D",
       url='https://github.com/FreeOpcUa/opcua-client-gui',
       packages=["uaclient"],
       license="GNU General Public License",
-      install_requires=["opcua>0.95.1", "opcua-widgets>0.5.0"],
+      install_requires=["opcua>=0.95.1", "opcua-widgets>=0.5.0"],
       entry_points={'console_scripts':
                     ['opcua-client = uaclient.mainwindow:main']
                     }
