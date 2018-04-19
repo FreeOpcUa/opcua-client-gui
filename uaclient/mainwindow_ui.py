5895de7a801d50b95b477d34dd8d8f68c2937fed
oroulet <olivier@loke>
2018-04-19 16:53:13 +0200

    fix Makefile

--- uaclient/mainwindow_ui.py
+++ uaclient/mainwindow_ui.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'mainwindow_ui.ui'
+# Form implementation generated from reading ui file 'uaclient/mainwindow_ui.ui'
 #
-# Created by: PyQt5 UI code generator 5.9
+# Created by: PyQt5 UI code generator 5.7
 #
 # WARNING! All changes made in this file will be lost!
 
