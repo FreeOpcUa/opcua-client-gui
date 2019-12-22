e710e718b739a2b80d29318816a6a0e8901730b9
oroulet <olivier@loke>
2019-12-22 20:40:21 +0100

    add snapcraft.yaml is someone wants a snap

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(name="opcua-client",
       version="0.8.0",
-      description="Minimal OPC-UA Client GUI",
+      description="OPC-UA Client GUI",
       author="Olivier R-D",
       url='https://github.com/FreeOpcUa/opcua-client-gui',
       packages=["uaclient"],
