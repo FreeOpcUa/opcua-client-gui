4d3a63a1d31ef2a2b8667e618b6c52d4c49a3e32
oroulet <olivier@r-dt.net>
2021-12-06 11:24:22 +0100

    make sure theme folder is packaged

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name="opcua-client",
       description="OPC-UA Client GUI",
       author="Olivier R-D",
       url='https://github.com/FreeOpcUa/opcua-client-gui',
-      packages=["uaclient"],
+      packages=["uaclient", "uaclient.theme"],
       license="GNU General Public License",
       install_requires=["asyncua", "opcua-widgets>=0.6.0", "PyQt5"],
       entry_points={'console_scripts':
