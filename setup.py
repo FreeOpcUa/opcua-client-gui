2fb29c5036702ab230009ee6f9316e10c3419d3d
oroulet <olivier@r-dt.net>
2021-12-06 08:25:36 +0100

    new release

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 
 setup(name="opcua-client",
-      version="0.8.0",
+      version="0.8.2",
       description="OPC-UA Client GUI",
       author="Olivier R-D",
       url='https://github.com/FreeOpcUa/opcua-client-gui',
       packages=["uaclient"],
       license="GNU General Public License",
-      install_requires=["asyncua", "opcua-widgets>=0.5.4", "PyQt5"],
+      install_requires=["asyncua", "opcua-widgets>=0.6.0", "PyQt5"],
       entry_points={'console_scripts':
                     ['opcua-client = uaclient.mainwindow:main']
                     }
