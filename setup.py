28a9d5d681c850efabe572f32a9f8d383705726f
aqua <trungnguyen241@gmail.com>
2026-02-11 13:11:56 -0600

    Update opc explorer setup to version 0.9.0 to match up with the latest upstream tag.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="opc-explorer",
-    version="0.8.4",
+    version="0.9.0",
     description="OPC Explorer",
     author="Key Technology",
     python_requires=">=3.8,<3.12",  # Only expand this as we add tests for them
